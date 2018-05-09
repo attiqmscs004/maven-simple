5e0cc7dc4e9673675694b6ba0677958dc1731f4c
attiqmscs004 <32477903+attiqmscs004@users.noreply.github.com>
2018-05-09 16:44:12 +0500

    Update and rename App.java to HelloWorld.java

---
+++ src/main/java/com/github/jitpack/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.github.jitpack;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld 
+{
+    public static void main(String[] args)
+    {
+        Greetings obj = new Greetings()
+        System.out.println(obj.greet("Jenkins"));
+    
+    }
+}
